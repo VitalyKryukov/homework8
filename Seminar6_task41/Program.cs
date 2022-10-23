d50a01a0365c21f0ba12c243b371689b68b85a0e
Vitaly Kryukov <v.b.kryukov@mail.ru>
2022-10-24 01:16:33 +0300

    Урок 6. Двумерные массивы и рекурсия. Домашнее задание

---
+++ Seminar6_task41/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Main();
+
+//Методы
+
+void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Start");
+    Console.WriteLine("Сколько чисел Вы планируете ввести: ");
+    int quantity = int.Parse(Console.ReadLine());
+    int[] array = ArrayNumbers(quantity);
+    PrintArrayNumbers(array);
+    Console.WriteLine($"-> {CountPositiveNumbers(array)}");
+    Console.WriteLine("End");
+}
+
+int[] ArrayNumbers(int size)
+{
+    Console.WriteLine("Введите числа: ");
+    int[] arrayM = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arrayM[i] = int.Parse(Console.ReadLine());
+    }
+    return arrayM;
+}
+
+
+void PrintArrayNumbers(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+        if (i != arr.Length - 1) Console.Write(", ");
+    }   
+}
+
+int CountPositiveNumbers(int[] inArray)
+{
+    int result = 0;
+    foreach (int el in inArray)
+    {
+        if (el > 0)
+        {
+            result += 1;
+        }
+    }
+    return result;
+}
